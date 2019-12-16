36cede331b3450d3a12733691bf31a672c236267
健 鈴木 <170430@jc-21.jp>
2019-12-16 11:35:02 +0900

    もう一回

--- R01_BMI/MainPage.xaml.cs
+++ R01_BMI/MainPage.xaml.cs
@@ -20,12 +20,13 @@ namespace R01_BMI
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            
             double cm = double.Parse(CM.Text);
             double kg = double.Parse(KG.Text);
             double a = cm / 100;
             double c = kg / (a * a);
             double rsl = Math.Round(c);
-            DisplayAlert("結果", "あなたのBMIは" + rsl + "です！", "OK");
+            string kekka = rsl.ToString();
         }
     }
 }
